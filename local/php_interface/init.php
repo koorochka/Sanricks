d74b9f3dcd3ecf589d7acfe32fe880071ab167d5
Koorochka <artemij@koorochka.ru>
2018-08-28 19:00:19 +0300

    Event OnBeforeOrderAdd

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,204 +1,68 @@
 <?
-AddEventHandler('main', 'OnBeforeEventSend', "sanricksBeforeEventSend");
-AddEventHandler("main", "OnAfterUserUpdate", "OnAfterUserUpdateHandler");
+use Bitrix\Main\EventManager;
+$eventManager = EventManager::getInstance();
+/**
+ * Add events handlers
+ */
+$eventManager->AddEventHandler("sale", "OnBeforeOrderAdd", "OnBeforeOrderAddHandler");
+/**
+ * Sanricks events handlers
+ */
 
 /**
- * Перед отправкой почтового сообщения сайта
+ * Вызывается перед добавлением заказа, может быть использовано для отмены или модификации данных.
+ * Метод CSaleOrder::Add
  * @param $arFields
- * @param $arTemplate
  */
-function sanricksBeforeEventSend(&$arFields, $arTemplate)
+function OnBeforeOrderAddHandler(&$arFields) {
+    AddMessage2Log($arFields);
+
+    // Для отмены
+    //return false;
+}
+
+/**
+ * Dev tools
+ */
+if (!function_exists("d") )
 {
-
-    if($arTemplate["EVENT_NAME"] == "NEW_USER"){
-        /**
-         * Данные менеджера
-         */
-        $rsUserI = CUser::GetList($by, $order, Array("ID" => $arFields["USER_ID"]), array("SELECT" => array("UF_*")));
-        if ($arUserI = $rsUserI->Fetch()) {
-            $fields = $arUserI;
-
-            if (!$fields["UF_MANAGER"])
-                $fields["UF_MANAGER"] = 443;
-
-            if ($fields["UF_MANAGER"]) {
-
-                if(CModule::IncludeModule("iblock")){
-                    $manager = CIBlockElement::GetList(
-                        false,
-                        array("IBLOCK_ID" => 10, "ACTIVE" => "Y", "ID" => $fields["UF_MANAGER"]),
-                        false,
-                        array("nPageSize" => 1),
-                        array("ID", "NAME", "PROPERTY_EMA")
-                    );
-                    if ($manager = $manager->Fetch()) {
-                        $fields["MANAGER_EMAIL"] = $manager["PROPERTY_EMA_VALUE"];
-                        $fields["MANAGER_NAME"] = $manager["NAME"];
-
-                    }
-                }
-            }
-
-            CEvent::Send("VJWEB_USER_ACTIVATE", "s1", $fields);
-
-            /**
-             * Deactivate
-             */
-            $user = new CUser();
-            $user->Update($arFields["USER_ID"], array("ACTIVE" => "N"));
+    function d($value, $type="pre")
+    {
+        if ( is_array( $value ) || is_object( $value ) )
+        {
+            echo "<" . $type . " class=\"prettyprint\">".htmlspecialcharsbx( print_r($value, true) )."</" . $type . ">";
+        }
+        else
+        {
+            echo "<" . $type . " class=\"prettyprint\">".htmlspecialcharsbx($value)."</" . $type . ">";
         }
     }
 }
 
 /**
- * @param $arFields
+ * @param $text
+ * @param array $limit
  */
-function OnAfterUserUpdateHandler(&$arFields)
-{
-    /**
-     * Notify user about activation
-     */
-    if($arFields["ACTIVE"] == "Y"){
-        $user = \Bitrix\Main\UserTable::getList(array(
-            "filter" => array(
-                "ID" => $arFields["ID"]
-            ),
-            "select" => array(
-                "ID",
-                "EMAIL",
-                "NAME",
-                "LAST_NAME"
-            )
-        ));
-        if($user = $user->fetch()){
-            CEvent::Send("USER_ACTIVATE_NOTIFY", "s1", array(
-                "EMAIL" => $user["EMAIL"],
-                "NAME" => $user["NAME"],
-                "LAST_NAME" => $user["LAST_NAME"]
-            ));
+function sanricksTextSizer($text, $limit=array()){
+    if(empty($limit)){
+        $limit = array(
+            6 => 10,
+            10 => 9,
+            15 => 8
+        );
+    }
+    $textSize = strlen($text);
+    $textFont = 0;
+    foreach ($limit as $size=>$value) {
+        if($textSize > $size){
+            $textFont = $value;
         }
     }
-}
 
-
-use Bitrix\Main\Entity;
-/**
- * Class OrderNamesTable
- */
-class OrderNamesTable extends Entity\DataManager {
-
-    /**
-     * @return string
-     */
-    public static function getFilePath() {
-        return __FILE__;
-    }
-
-    /**
-     * @return string
-     */
-    public static function getTableName() {
-        return 'b_hlbd_order';
-    }
-
-    /**
-     * @return array
-     */
-    public static function getMap() {
-        return [
-            new Entity\IntegerField('id', [
-                'primary' => true,
-                'column_name' => 'ID',
-                'autocomplete' => true
-            ]),
-            new Entity\IntegerField('orderId', [
-                'column_name' => 'UF_ID',
-                'required' => true,
-                'title' => 'Номер заказа'
-            ]),
-            new Entity\StringField('name', [
-                'column_name' => 'UF_NAME',
-                'required' => true,
-                'title' => 'Наименование заказа'
-            ]),
-            new Entity\StringField('xmlId', [
-                'column_name' => 'UF_XML_ID',
-                'required' => true,
-                'title' => 'Внешний идентификатор обмена'
-            ])
-        ];
+    if($textFont > 0){
+        ?><div class="text-<?=$textFont?>"><?=$text?></div><?
+    }else{
+        echo $text;
     }
 }
-
-/**
- * Class TagsTable
- */
-class TagsTable extends Entity\DataManager {
-
-    /**
-     * @return string
-     */
-    public static function getFilePath() {
-        return __FILE__;
-    }
-
-    /**
-     * @return string
-     */
-    public static function getTableName() {
-        return 'b_hlbd_metki';
-    }
-
-    /**
-     * @return array
-     */
-    public static function getMap() {
-        return [
-            new Entity\IntegerField('id', [
-                'primary' => true,
-                'column_name' => 'ID',
-                'autocomplete' => true
-            ]),
-            new Entity\IntegerField('fileId', [
-                'column_name' => 'UF_FILE',
-                'required' => true,
-                'title' => 'Файл'
-            ]),
-            new Entity\StringField('def', [
-                'column_name' => 'UF_DEF',
-                'required' => true,
-                'title' => 'def'
-            ]),
-            new Entity\StringField('xmlId', [
-                'column_name' => 'UF_XML_ID',
-                'required' => true,
-                'title' => 'Внешний идентификатор обмена'
-            ]),
-            new Entity\StringField('fd', [
-                'column_name' => 'UF_FULL_DESCRIPTION',
-                'required' => true,
-                'title' => 'fd'
-            ]),
-            new Entity\StringField('desc', [
-                'column_name' => 'UF_DESCRIPTION',
-                'required' => true,
-                'title' => 'desc'
-            ]),
-            new Entity\StringField('link', [
-                'column_name' => 'UF_LINK',
-                'required' => true,
-                'title' => 'link'
-            ]),
-            new Entity\StringField('sort', [
-                'column_name' => 'UF_SORT',
-                'required' => true,
-                'title' => 'sort'
-            ]),
-            new Entity\StringField('name', [
-                'column_name' => 'UF_NAME',
-                'required' => true,
-                'title' => 'name'
-            ])
-        ];
-    }
-}
+?>
